d78daec82a2c1ee9c3665fdd81bb628ab278cc46
hongbi-lee <foxgirin950@gmail.com>
2022-04-04 01:33:48 +0900

    done with chapter 3

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickButton = findViewById<Button>(R.id.clickButton)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        clickButton.setOnClickListener {
+            textView.text = "버튼을 눌렀습니다"
+        }
     }
 }
