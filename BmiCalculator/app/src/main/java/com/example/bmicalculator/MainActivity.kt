ba8da75f8b9c23b846d66788d8cc4a2b4241b052
hongbi-lee <foxgirin950@gmail.com>
2022-05-01 19:27:49 +0900

    done with page 148

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bmicalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.resultButton.setOnClickListener {
+            // 결과 버튼이 클릭되면 할 일을 작성하는 부분
+            if (binding.weightEditText.text.isNotBlank() && binding.heightEditText.text.isNotBlank()) {
+                val intent = Intent(this, ResultActivity::class.java).apply {
+                    putExtra("weight", binding.weightEditText.text.toString().toFloat())
+                    putExtra("height", binding.heightEditText.text.toString().toFloat())
+                }
+                startActivity(intent)
+            }
+        }
     }
 }
