32c189dca281c03a516a5f16fa27fe2d5e880b1e
hongbi-lee <foxgirin950@gmail.com>
2022-05-09 00:17:20 +0900

    add code for events when clicking menus

--- MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mywebbrowser
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -44,5 +48,47 @@ class MainActivity : AppCompatActivity() {
         }else {
             super.onBackPressed()   // 원래 동작 수행
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                binding.webView.loadUrl("https://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                binding.webView.loadUrl("https://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                binding.webView.loadUrl("https://www.daum.net")
+                return true
+            }
+            R.id.action_call -> {   // 암시적 인텐트
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:031-123-4567")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                binding.webView.url?.let {
+                    url -> // 문자보내기
+                }
+            }
+            R.id.action_email -> {
+                binding.webView.url?.let {
+                    url -> // 이메일 보내기
+                }
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)    // 처리하고자 하는 경우를 제외한 그 이외의 경우 super 메서드를 호출하는 것이 안드로이드 시스템에서의 보편적인 규칙
     }
 }
