0b109f45d5c0750478d3033bebcd87ac47944b22
hongbi-lee <foxgirin950@gmail.com>
2022-05-22 23:31:50 +0900

    add TorchAppWidget

--- Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flashlight
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.flashlight.databinding.ActivityMainBinding
@@ -11,6 +12,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.flashSwitch.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                startService(Intent(this, TorchService::class.java).apply {
+                    action = "on"
+                })
+            } else {
+                startService(Intent(this, TorchService::class.java).apply {
+                    action = "off"
+                })
+            }
+        }
     }
 }
