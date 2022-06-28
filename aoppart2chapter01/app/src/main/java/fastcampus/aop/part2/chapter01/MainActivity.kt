a60c72546ab2817feb526cc08774a83f6a7bc9ec
hongbi-lee <foxgirin950@gmail.com>
2022-06-29 00:59:07 +0900

    create ResultActivity and connect

--- aoppart2chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/MainActivity.kt
+++ aoppart2chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/MainActivity.kt
@@ -1,11 +1,37 @@
 package fastcampus.aop.part2.chapter01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener { // resultButton을 눌렀을 때 기능 정립
+            // Log.d("MainActivity", "ResultButton이 클릭되었습니다.")
+
+            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            // 빈 값이 아닌 경우
+            val height: Int = heightEditText.text.toString().toInt()
+            val weight: Int = weightEditText.text.toString().toInt()
+
+            // Log.d("MainActivity", "height: $height weight: $weight")
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
